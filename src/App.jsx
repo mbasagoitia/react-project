458629a28a1f85e14afa6f738b31d3398239f5dc
basagoitiam <marika.basagoitia@gmail.com>
2022-10-25 10:36:48 -0400

    finished todoList exercise

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Heading from './heading';
+import Paragraph from './paragraph';
+
+class App extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = {
+      list: ["ready", "set", "GO"],
+      text: "",
+    }
+
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      list: [...this.state.list, this.state.text],
+      text: "",
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello World</h1>
+        <form onSubmit = {this.onSubmit}>
+          <input type="text" value = {this.state.text} onChange = {(event) => {
+            this.setState({
+              text: event.target.value,
+            })
+          }}/>
+          <button type="submit">Add</button>
+        </form>
+        <ul>{this.state.list.map((item, index) => {
+          return <li key = {index}>{item}</li>
+        })}</ul>
+      </div>
+    );
+  }
+}
+
+export default App;
--- src/heading.js
+++ src/heading.js
@@ -1,6 +1,10 @@
 import React, { Component } from "react";
 
 class Heading extends Component {
+    constructor (props) {
+        super (props);
+    }
+    
     render () {
         return <h1>This is my {this.props.number} heading.</h1>
     }
